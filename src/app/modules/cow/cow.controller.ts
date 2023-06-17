a619e431677492f34c28fef584772e52373423dc
sabbir <sabbir@gmail.com>
2023-06-17 22:23:28 +0600

    create cow controller

---
+++ src/app/modules/cow/cow.controller.ts
@@ -0,0 +1,24 @@
+import httpStatus from "http-status";
+import sendResponse from "../../../shared/sendResponse";
+import { NextFunction, Request, Response } from "express";
+import { CowService } from "./cow.service";
+
+const createCow = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { cow } = req.body;
+    const result = await CowService.createCow(cow);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "user created",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const CowController = {
+  createCow,
+};
